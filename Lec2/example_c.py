ba615c9cd6a59aada1cdc84d14aff37112fa8fc0
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-11-08 12:15:32 +0300

    Обновление лекции 2

---
+++ Lec2/example_c.py
@@ -0,0 +1,15 @@
+# Вывод нескольких значений за один print()
+print("Bob", "likes", "Baseball")
+
+
+# Вывод значений переменных
+name = "Bob"
+print("My name is:", name) # print("My names is:", "Bob")
+
+
+# Вывод нескольких значений переменных
+first_name = "Bob"
+last_name = "Johnson"
+age = "19"
+
+print("Full info:", first_name, last_name, "and age:", age)
