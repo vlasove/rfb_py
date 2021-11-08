8fadea9b5619cac2f07092f91af34ab2db3390bd
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-11-08 12:57:58 +0300

    Обновление лекции 2

---
+++ Lec2/example_d.py
@@ -0,0 +1,6 @@
+# Работа с StdIn
+first_name = input() # "Vasya" => input() оставляет вместо себя строки
+last_name = input() # "Petrov"
+age = input() # "22"
+
+print(f"Full Name: {first_name} {last_name}. Age is {age}")
