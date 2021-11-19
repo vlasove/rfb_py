f70f31e3c29adc7c6ecec3889bbbd89a4026db10
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-11-19 11:37:57 +0300

    Лекция 10 обновление

---
+++ Lec10/example_b.py
@@ -0,0 +1,20 @@
+# Задача E. Староста
+
+n_days = int(input())
+total_set = set()
+
+
+for day in range(n_days):
+    m_students_in_this_day = int(input())
+    set_students_in_ths_day = set()
+
+    for _ in range(m_students_in_this_day):
+        student = input()
+        set_students_in_ths_day.add(student)
+    
+    if day == 0:
+        total_set = total_set.union(set_students_in_ths_day)
+    else:
+        ...
+
+# Дописать
