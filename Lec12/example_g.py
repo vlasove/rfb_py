934a3280cd6ae3b56925143a8184520375547b9f
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-11-22 12:50:29 +0300

    Лекция 12 обновление

---
+++ Lec12/example_g.py
@@ -0,0 +1,10 @@
+# Сортировка списка
+nums = [10, 2, 3, 4, 12, 11, -2, 700, 53]
+nums.sort(reverse=True)
+print("Sorted nums:", nums)
+
+# Сортировка списка с созданием нового
+numerics = [10, 2, 3, 4, 12, 11, -2, 700, 53]
+sorted_numerics = sorted(numerics, reverse=True)
+print("Sorted numerics:", sorted_numerics)
+print("Origin numerics:", numerics)
---
+++ Lec12/example_h.py
@@ -0,0 +1,8 @@
+# Первращение списка во множество
+nums = [10, 20, 10, 20, 30, 40]
+nums_set = set(nums)
+print("Nums set:", nums_set)
+
+# Превращение множества в список
+nums_list = list(nums_set)
+print("Nums list:", nums_list)
