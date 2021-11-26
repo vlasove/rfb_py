d8963ee65f89b78d12f221e91c12b783937b79d8
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-11-26 12:09:24 +0300

    Лекция 14 обновление

---
+++ Lec14/example_g.py
@@ -0,0 +1,19 @@
+# Задача B. Очередь
+
+n = int(input().strip())
+events = [] # Список событий
+for _ in range(n):
+    events.append(input().strip())
+
+queue = [] # Моя очередь - тут будут только фамилии
+for event in events:
+    if "Следующий" in event:
+        if len(queue) > 0 :
+            person = queue.pop(0) # По умолчанию удаляет последний!
+                                  # если передан индекс - то выкинет элемент с индексом
+            print(f"Заходит {person}!")
+        else:
+            print("В очереди никого нет.")
+    elif "Кто последний?" in event:
+        temp_lst = event.split(" - ") # ["Кто последний? Я", "Кузнецов."]
+        queue.append(temp_lst[-1].split(".")[0]) # ["Кузнецов", ""][0]
