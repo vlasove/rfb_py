cad627dd167845a76c917fb965c4962aeb92592d
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-01 11:18:06 +0300

    Лекция 18

---
+++ Lec18/example_a.py
@@ -0,0 +1,30 @@
+# 1. Тип аргументов - required positional aruments 
+# необходимые позициональные аргуементы
+def sub_squared(a, b):
+    res = a ** 2 - 2*a*b - b ** 2
+    return res
+
+print(sub_squared(3,4))
+print(sub_squared(4,3))
+#print(sub_squared(10, 10, 10))
+
+# 2. Пример использования функций с rp-аргументами
+def print_factorials(start, stop):
+    for j in range(start, stop + 1):
+        print(f"{j}! = {factorial(j)}")
+
+def factorial(n):
+    if n < 0:
+        return None
+    if n <= 1:
+        return 1
+    res = 1
+    for i in range(2, n+1):
+        res *= i
+    return res
+
+
+print_factorials(1, 15)
+
+
+    
